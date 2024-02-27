3bbca0c40506e6084542574cfd33297da482b999
noeliamorato <cbbe.noelia.morato.ca@unifranz.edu.bo>
2024-02-27 14:38:54 -0400

    Vs de ambas imágenes

--- rotacion.py
+++ rotacion.py
@@ -5,7 +5,8 @@ rows, cols = img.shape
 
 M = cv2.getRotationMatrix2D((cols/2, rows/2), 45, 1)
 dst = cv2.warpAffine(img, M, (cols, rows))
+concatenated_img = cv2.hconcat([img, dst])
 
-cv2.imshow('Rotación', dst)
+cv2.imshow('Original vs Rotada', concatenated_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- traslacion.py
+++ traslacion.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
- 
-img = cv2.imread('Llamame-Gatito.jpg',0)
-rows,cols = img.shape
- 
-M = np.float32([[1,0,210],[0,1,20]])
-dst = cv2.warpAffine(img,M,(cols,rows))
- 
-cv2.imshow('Traslación',dst)
+
+img = cv2.imread('Llamame-Gatito.jpg', 0)
+rows, cols = img.shape
+
+M = np.float32([[1, 0, 210], [0, 1, 20]])
+dst = cv2.warpAffine(img, M, (cols, rows))
+concatenated_img = cv2.hconcat([img, dst])
+
+cv2.imshow('Original vs Traslación', concatenated_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
